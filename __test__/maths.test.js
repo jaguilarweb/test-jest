16ea56e078546b69e8a671b21d24cc4b9a13f563
Jenny Aguilar <contact@jaguilarweb.com>
2021-05-11 23:27:10 -0400

    test: Add basic test for math functions

--- __test__/maths.test.js
+++ __test__/maths.test.js
@@ -1,2 +1,17 @@
+import { sumar, multiplicar, restar, dividir } from "../maths.js";
 
+describe('Cálculos matemáticos', () => {
+  test('Pruebas de sumas', () => {
+    expect(sumar(1,1)).toBe(3);
+  });
+  test('Multiplicar', () => {
+    expect(multiplicar(2,4)).toBe(8);
+  });
+  test('Dividir', () => {
+    expect(dividir(4,2)).toBe(2);
+  });
+  test('Restar', () => {
+    expect(restar(2,4)).toBe(3);
+  });
+}); 
 
