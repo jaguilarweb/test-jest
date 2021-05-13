441a9e24ae48a2c923bc8270f5f738a2f07ac0eb
Jenny Aguilar <contact@jaguilarweb.com>
2021-05-13 12:37:53 -0400

    test: Add Async/Await with error handler test

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -1,6 +1,6 @@
 import { getDataFromApi } from "../promise";
 
-describe('Probar Async/Await', () => {
+describe('Probar Async/Await CURSO', () => {
   test('Realizar una petición a una API', async () => {
     const api = 'https://rickandmortyapi.com/api/character/';
     const getRick = 'https://rickandmortyapi.com/api/character/1';
@@ -11,4 +11,60 @@ describe('Probar Async/Await', () => {
       expect(data.name).toEqual('Rick Sanchez');
     });
   });
+  test('Probar Async/Await curso con error', async () => {
+    const apiError = "http://httpstat.us/404";
+    const peticion = getDataFromApi(apiError);
+    await expect(peticion).rejects.toEqual(Error('Request failed with status code 404'));
+  });
+  test('Probar Async/Await reject', async () => {
+    const apiError = "http://httpstat.us/404";
+    const peticion = getDataFromApi(apiError);
+    await expect(Promise.resolve('Hola')).resolves.toBe('Hola');
+    await expect(Promise.reject('Error')).rejects.toBe('Error');
+  });
 });
+
+
+/* Async/Await no es necesario incluir el then, los siguientes son ejemplos
+conforme a la documentación */
+
+describe('Probar Async/Await DOC', () => {
+  test('Realizar una petición a una API', async () => {
+    const api = 'https://rickandmortyapi.com/api/character/';
+    const data = await getDataFromApi(api);
+    expect(data.results.length).toBeGreaterThan(0);
+  });
+});
+
+// Este es el ejemplo de la documentación
+describe('Probar Async/Await try/catch', () => {
+  function fetchDataPromise() {
+    return new Promise(resolve => {
+      setTimeout( () => {
+        resolve("peanut butter")
+      }, 100);
+    });
+  }
+  function fetchDataPromiseError() {
+    return new Promise((resolve, reject) => {
+      setTimeout( () => {
+        reject("error")
+      }, 100);
+    });
+  }
+  
+  test('the data is peanut butter', async () => {
+    const data = await fetchDataPromise();
+    expect(data).toBe("peanut butter");
+  });
+  
+  test('the fetch fails with an error', async () => {
+    expect.assertions(1);
+    try {
+      await fetchDataPromiseError();
+    } catch (e) {
+      expect(e).toMatch("error");
+    }
+  });
+});
+
