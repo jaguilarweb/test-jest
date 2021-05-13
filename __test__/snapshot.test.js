7783b0420b5788a845ffcab815dfc4a771353d8b
Jenny Aguilar <contact@jaguilarweb.com>
2021-05-13 14:34:52 -0400

    test: Add exception for snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -7,4 +7,21 @@ describe('Es hora de las instantaneas', () => {
       expect(getCharacter(rick)).toMatchSnapshot();
     });
 
+    test('Siempre fallara la instantanea', () => {
+      const user = {
+        createAt: new Date(),
+        id: Math.floor(Math.random() * 20),
+      }
+      expect(user).toMatchSnapshot();
+    });
+
+    test('Tenemos una excepción dentro del código', () => {
+      const user = {
+        id: Math.floor(Math.random() * 20),
+        name: "Jenny Aguilar"
+      }
+      expect(user).toMatchSnapshot({
+        id: expect.any(Number)
+      });
+    });
 });
