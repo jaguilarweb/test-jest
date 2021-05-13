207f8e5a810c11abe4c03d5eff467f45515d5be7
Jenny Aguilar <contact@jaguilarweb.com>
2021-05-12 23:25:06 -0400

    test: Add resolve and reject promise  test

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -8,4 +8,11 @@ describe('Probando promesas', () => {
       done();
     });
   });
+  test('Resuelve un Hola', () => {
+    return expect(Promise.resolve('Hola')).resolves.toBe('Hola');
+  });
+  test('Rechasa con un error', () => {
+    return expect(Promise.reject('Errror')).rejects.toBe('Errror');
+  });
+
 });
